2f249457bcbf8428ceb8046a7eb188a4dfc8513f
LeoTz <108926441+LeoTz@users.noreply.github.com>
2025-10-22 13:04:42 +0400

    Replace sequential auth with PIN-based authentication
    Updated main menu to launch PIN-based authentication instead of sequential interaction authentication. Removed sequential_interaction_authentication.tscn and added pin_based_authentication.tscn. Updated button labels and signal connections in MainMenuUI and main_menu.gd accordingly. Minor cleanup in xr_origin.tscn.

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,13 +1,14 @@
 extends Control
 
-func _on_sequential_button_pressed() -> void:
-	print("Sequential Button pressed!")
+func _on_pin_button_pressed() -> void:
+	print("PIN Button pressed!")
 	var main = get_tree().root.get_node_or_null("Main")
 	if main:
-		main.change_to_scene("res://scenes/sequential_interaction_authentication.tscn")
+		main.change_to_scene("res://scenes/pin_based_authentication.tscn")
 
 func _on_spatial_button_pressed() -> void:
 	print("Spatial Button pressed!")
 	var main = get_tree().root.get_node_or_null("Main")
 	if main:
 		main.change_to_scene("res://scenes/spatial_arrangement_authentication.tscn")
+		
