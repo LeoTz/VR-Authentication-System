82ff303ecf76a0a836544c630e5f6be9215057c4
Eyad <eyadnasr8@gmail.com>
2025-10-24 16:26:05 +0400

    Added pickablility to shapes

--- scripts/spatial_arrangment/shape.gd
+++ scripts/spatial_arrangment/shape.gd
@@ -2,6 +2,11 @@
 extends Node3D
 class_name Shape
 
+const CUBE = preload("uid://cy2q6kfcih4gr")
+const TORUS = preload("uid://dxs4dp7ewbwg8")
+const SPHERE = preload("uid://h5ixyu701hg7")
+const CYLINDER = preload("uid://fa52g5uavh3n")
+
 @export var data: ShapeData:
 	set(value):
 		data = value.duplicate()
@@ -30,35 +35,41 @@ func _create_shape(type : ShapeData.Types, color : ShapeData.Colors):
 	for child in get_children():
 		child.queue_free()
 	
-	var shape: CSGPrimitive3D
+	var shape: XRToolsPickable
 	
 	# Create the right shape
 	match type:
 		ShapeData.Types.CUBE:
-			shape = CSGBox3D.new()
+			shape = CUBE.instantiate()
 		ShapeData.Types.CYLINDER:
-			shape = CSGCylinder3D.new()
-			shape.height = 1.0
+			shape = CYLINDER.instantiate()
 		ShapeData.Types.SPHERE:
-			shape = CSGSphere3D.new()
+			shape = SPHERE.instantiate()
 		ShapeData.Types.TORUS:
-			shape = CSGTorus3D.new()
+			shape = TORUS.instantiate()
 		ShapeData.Types.EMPTY:
 			shape = null
 	
 	if shape:
-		shape.scale = Vector3(0.3, 0.3, 0.3)
+		shape.connect('grabbed', test)
+		# Disable physics
+		#shape.freeze = true
+		
 		# Apply the color
 		var mat := StandardMaterial3D.new()
 		mat.albedo_color = _get_color_from_enum(color)
-		shape.material = mat
+		shape.get_child(0).material = mat
 		
 		add_child(shape)
 		
+		# make persistent in editor
 		if Engine.is_editor_hint():
-			shape.owner = get_tree().edited_scene_root  # make persistent in editor
+			shape.owner = get_tree().edited_scene_root 
 
 func _apply_data():
 	if not is_inside_tree():
 		return
 	_create_shape(data.type, data.color)
+
+func test():
+	print("grab")
